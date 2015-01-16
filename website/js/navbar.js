82f0bfe68c19a321bed61524a30f1588c457b461
felisadae <felisadae@gmail.com>
2015-01-16 17:39:58 -0600

    Added navbar and consolidated relevant css files into styles.css

---
+++ website/js/navbar.js
@@ -0,0 +1,12 @@
+<script>
+	// Fix Navigation on Scroll
+	$(window).scroll(function(){
+      	if ($(this).scrollTop() > 120) {
+          	$('.navbar').addClass('fix-scroll');
+          	$('.navbar-fix').addClass('space');
+      	} else {
+          	$('.navbar').removeClass('fix-scroll');
+          	$('.navbar-fix').removeClass('space');
+      	}
+  	});
+    </script>
