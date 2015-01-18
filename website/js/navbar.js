8b672d59c1128ec72761758e96593509c254d7bd
felisadae <felisadae@gmail.com>
2015-01-18 01:35:30 -0600

    Fixed navbar links

--- website/js/navbar.js
+++ website/js/navbar.js
@@ -1,12 +1,10 @@
-<script>
-	// Fix Navigation on Scroll
-	$(window).scroll(function(){
-      	if ($(this).scrollTop() > 120) {
-          	$('.navbar').addClass('fix-scroll');
-          	$('.navbar-fix').addClass('space');
-      	} else {
-          	$('.navbar').removeClass('fix-scroll');
-          	$('.navbar-fix').removeClass('space');
-      	}
-  	});
-    </script>
+// Fix Navigation on Scroll
+$(window).scroll(function(){
+    	if ($(this).scrollTop() > 120) {
+        	$('.navbar').addClass('fix-scroll');
+        	$('.navbar-fix').addClass('space');
+    	} else {
+        	$('.navbar').removeClass('fix-scroll');
+        	$('.navbar-fix').removeClass('space');
+    	}
+	});
