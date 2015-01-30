6433e5ae7404fe6026b0b70f4368e211e1f94f8f
Rice Apps <riceapps@riceapps.org>
2015-01-30 06:07:43 -0500

    ONLY NEED TO FIX THE MINITASKS + some design issues + contact page mail connect

--- app.py
+++ app.py
@@ -13,15 +13,21 @@ app.config.from_object(__name__)
 # Email
 mail = Mail(app)
 
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
 # Database
 @app.before_request
 def before_request():
-	g.db = sql.connect('apply.db', detect_types=sql.PARSE_DECLTYPES) #check_same_thread=False
+	g.con = sql.connect('apply.db', detect_types=sql.PARSE_DECLTYPES) #check_same_thread=False
+	g.con.row_factory = make_dicts
+	g.cur = g.con.cursor()
 
 @app.teardown_request
 def teardown_request(exception):
-	if hasattr(g, 'db'):
-		g.db.close()
+	if hasattr(g, 'con'):
+		g.con.close()
 
 @app.route("/")
 def index():
@@ -55,8 +61,7 @@ def contact():
 		email = request.form['email']
 		text = request.form['text']
 		subject = ('Bug' if request.form['type'] == 'bug' else 'Idea') + ' Submission by %s' % name 
-		print subject + "SHPGHPOIHEFPOQIHEG"
-		msg = Message(text, sender=email, recipients=['xal1@rice.edu'], subject=subject)
+		msg = Message(body=text, sender=email, recipients=['xal1@rice.edu'], subject=subject)
 		mail.send(msg)
 		return redirect('/contact')
 
@@ -80,18 +85,18 @@ def apply():
 		name = request.form['name'] # applicant's name
 		email = request.form['email'] # applicant's rice email
 		grade = request.form['year'] # applicant's grade
-		position = request.form.getlist('position') # position applying for
-		project = request.form.getlist('project') # projects not desired
+		position = " ".join(request.form.getlist('position')) # position applying for
+		project = " ".join(request.form.getlist('project')) # projects not desired
 		skills = request.form['skills'] # applicant's skills
 		comments = request.form['comments'] # additional comments
-		task = request.form.getlist['mini_task'] # completed mini-task
-		with con:
-			g.db.execute("""INSERT INTO apply (name, email, phone, grade, major, position, project, skills, comments, task, timestamp) 
-							VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", 
-							(name, email, grade, major, position, project, skills, comments, task, datetime.now()))
-			g.db.commit()
+		task = " ".join(request.form.getlist('mini_task')) # completed mini-task
+		with g.con:
+			g.cur.execute("""INSERT INTO application (name, email, grade, position, project, skills, comments, task, timestamp) 
+							VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""", 
+							(name, email, grade, position, project, skills, comments, task, datetime.now()))
+			g.con.commit()
 		return redirect('/apply')
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=80, debug=True)
+	app.run(host="0.0.0.0", port=80)#, debug=True)
 
