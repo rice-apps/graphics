cc12b8fef1a49cca116aebde732f9730a4abf712
jeffreyxiong <jeffxiong017@gmail.com>
2015-02-14 16:14:04 -0600

    navbar stuff

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,6 @@
 // Fix Navigation on Scroll
 $(window).scroll(function(){
-    	if ($(this).scrollTop() > 150) {
+    	if ($(this).scrollTop() > 170) {
         	$('.navbar').addClass('fix-scroll');
         	$('.navbar-fix').addClass('space');
     	} else {
